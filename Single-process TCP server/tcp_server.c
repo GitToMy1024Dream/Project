ffa4de5108983c61717a8eff9239a5e4a76ec59a
GitToMy1024Dream <postmaster@1024do.com>
2017-07-04 00:02:10 +0800

    单进程TCP服务器

--- Single-process TCP server/tcp_server.c
+++ Single-process TCP server/tcp_server.c
@@ -48,19 +48,10 @@ int main(int argc,char* argv[])
 		if(new_sock < 0){
 			perror("accept");
 			continue;
-		}		
-		printf("get new client [%s:%d]\n",inet_ntoa(client.sin_addr),ntohs(client.sin_port));
-		
-		pid_t id = fork();
-		if(id < 0){
-			perror("fork");
-			close(new_sock);
 		}
-		else if(id == 0){
-			close(listen_sock);
-			if(fork() > 0){
-				exit(0);
-			}
+		
+		printf("get new client [%s:%d]\n",inet_ntoa(client.sin_addr),ntohs(client.sin_port));
+
 		//read->write
 		while(1){
 			char buf[1024];
@@ -71,23 +62,12 @@ int main(int argc,char* argv[])
 				printf("client# %s\n",buf);
 				write(new_sock,buf,strlen(buf));
 			}
-			else if(s == 0){
-				printf("client close!\n");
-				break;
-			}
 			else{
 				perror("read");
 				break;
 			}
 		}
 		close(new_sock);
-		exit(0);
-	}
-	else {
-		close(new_sock);
-		waitpid(id,NULL,0);
-	}
 	}
 	return 0;
 }
-
